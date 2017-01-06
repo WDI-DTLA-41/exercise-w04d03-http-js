10ba3b7be276a9f0d32734599e79c987dcfa8388
Andrew Maidah <a.maidah@gmail.com>
2017-01-05 17:10:44 -0800

    finishes trump as best actor of time

--- server.js
+++ server.js
@@ -1,13 +1,61 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
 
 var hostname = '127.0.0.1';
 var port = 3000;
 
+var routes = [
+  {
+    method: 'GET',
+    path: '/',
+    content: 'Hello'
+  },
+  {
+    method: 'GET',
+    path: '/about',
+    content: 'meow'
+  },
+  {
+    method: 'GET',
+    path: '/films/deal',
+    content: 'meow'
+  }
+];
+
+fs.readFile('./trump.txt', 'utf8', (err, data) => {
+  if (err) throw err;
+  routes[0].content = data;
+})
+
+fs.readFile('./bio.txt', 'utf8', (err, data) => {
+  if (err) throw err;
+  routes[1].content = data;
+})
+
+fs.readFile('./the-art-of-the-deal.txt', 'utf8', (err, data) => {
+  if (err) throw err;
+  routes[2].content = data;
+})
+
 var server = http.createServer(function(req, res) {
+  var method = req.method,
+      url = req.url;
   console.log('Incoming ', req.method, ' request to ', req.url);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  var action = routes.find(function(route) {
+    return route.method === method && route.path === url
+  });
+
+  if ( action ) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write( action.content );
+  }
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('uhoh');
+  }
+  res.end();
 });
 
 server.listen(port, hostname, function() {
