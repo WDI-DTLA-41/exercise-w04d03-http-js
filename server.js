4929f879306bc64aa69fca65fb9a8bc9268759b2
dakotah walker <walker.dakotah@gmail.com>
2017-01-05 19:27:17 -0800

    adds pages for each movie title

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 
 var hostname = '127.0.0.1';
 var port = 3000;
+var films = ['/newsies', '/american-psycho', '/the-dark-knight'];
 
 var server = http.createServer(function(req, res) {
   var method = req.method;
@@ -42,6 +43,33 @@ var server = http.createServer(function(req, res) {
       res.write(data);
       res.end();
     })
+  }
+  // create urls for each movie
+  else if (method === 'GET' && url === '/films' + films[0]) {
+    fs.readFile('./newsies.html', 'utf-8', function (err, data) {
+      if (err) throw err;
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.write(data);
+      res.end();
+    })
+  } else if (method === 'GET' && url === '/films' + films[1]) {
+    fs.readFile('./american-psycho.html', 'utf-8', function (err, data) {
+      if (err) throw err;
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.write(data);
+      res.end();
+    })
+  } else if (method === 'GET' && url === '/films' + films[2]) {
+    fs.readFile('./the-dark-knight.html', 'utf-8', function (err, data) {
+      if (err) throw err;
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.write(data);
+      res.end();
+    })
+  // return 404 error
   } else {
     res.statusCode = 404;
     res.write('try again');
@@ -52,3 +80,4 @@ var server = http.createServer(function(req, res) {
 server.listen(port, hostname, function() {
   console.log('Server running at http://' + hostname + ':' + port);
 });
+
