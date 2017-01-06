504012db35c6c6bcca9c1f605394f41e0b14202d
Justin Samuels <blizm87@gmail.com>
2017-01-05 18:49:44 -0800

    Completed exercise

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 var http = require('http');
+var fs = require('fs');
 
 var hostname = '127.0.0.1';
 var port = 3000;
 
-var server = http.createServer(function(req, res) {
-  console.log('Incoming ', req.method, ' request to ', req.url);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+var routes = [
+  { method: 'GET', path: '/', content: './index.html' },
+  { method: 'GET', path: '/about', content: './about.html' },
+  { method: 'GET', path: '/films/pitchblack', content: './films/pitchblack.html' }
+];
+
+var server = http.createServer(function(request, response) {
+  var method = request.method;
+  var url = request.url;
+  console.log('Incoming ', method, ' request to ', url);
+  var action = routes.find(function(route){
+    return route.method === method && route.path === url;
+  });
+  if (action){
+    fs.readFile( action.content, 'utf-8', function(err, data){
+      if(err) throw err;
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/html');
+      response.write(data);
+      response.end();
+    });
+  } else {
+      response.statusCode = 404;
+      response.setHeader('Content-Type', 'text/plain');
+      response.write('uh oh, page not found');
+      response.end();
+    }
 });
 
 server.listen(port, hostname, function() {
