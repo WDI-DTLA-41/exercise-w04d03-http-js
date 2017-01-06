6ef73ff3f0d2e3045ee8e411ed7d082185db6a84
Andrew Maidah <a.maidah@gmail.com>
2017-01-05 19:24:57 -0800

    fails at pulling files in dynamically

--- server.js
+++ server.js
@@ -8,31 +8,34 @@ var routes = [
   {
     method: 'GET',
     path: '/',
-    content: 'Hello'
+    content: 'Hello',
+    file: './trump.txt'
   },
   {
     method: 'GET',
     path: '/about',
-    content: 'meow'
+    content: 'meow',
+    file: './bio.txt'
   },
   {
     method: 'GET',
     path: '/films/deal',
-    content: 'meow'
+    content: 'meow',
+    file: './the-art-of-the-deal.txt'
   }
 ];
 
-fs.readFile('./trump.txt', 'utf8', (err, data) => {
+fs.readFile(routes[0].file, 'utf8', (err, data) => {
   if (err) throw err;
   routes[0].content = data;
 })
 
-fs.readFile('./bio.txt', 'utf8', (err, data) => {
+fs.readFile(routes[1].file, 'utf8', (err, data) => {
   if (err) throw err;
   routes[1].content = data;
 })
 
-fs.readFile('./the-art-of-the-deal.txt', 'utf8', (err, data) => {
+fs.readFile(routes[2].file, 'utf8', (err, data) => {
   if (err) throw err;
   routes[2].content = data;
 })
@@ -42,9 +45,8 @@ var server = http.createServer(function(req, res) {
       url = req.url;
   console.log('Incoming ', req.method, ' request to ', req.url);
   var action = routes.find(function(route) {
-    return route.method === method && route.path === url
+    return route.method === method && route.path === url;
   });
-
   if ( action ) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
@@ -61,3 +63,35 @@ var server = http.createServer(function(req, res) {
 server.listen(port, hostname, function() {
   console.log('Server running at http://' + hostname + ':' + port);
 });
+
+//dynamically call the file based on requested url
+
+//maybe use object.values()
+
+// var server = http.createServer(function(req, res) {
+//   var method = req.method,
+//       url = req.url;
+//   console.log('Incoming ', req.method, ' request to ', req.url);
+//   var action = routes.find(function(route) {
+//     return route.method === method && route.path === url;
+//   });
+//   if ( action ) {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     fs.readFile(action.file, 'utf8', (err, data) => {
+//       if (err) throw err;
+//       action.content = data;
+//       res.write( action.content );
+//     })
+//   }
+//   else {
+//     res.statusCode = 404;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('uhoh');
+//   }
+//   res.end();
+// });
+
+// server.listen(port, hostname, function() {
+//   console.log('Server running at http://' + hostname + ':' + port);
+// });
