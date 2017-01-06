b296285a6d23799243799846a5ce9b0cc32c0e2b
dakotah walker <walker.dakotah@gmail.com>
2017-01-05 16:19:31 -0800

    creates server and returns response to client

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 var http = require('http');
+var fs = require('fs');
 
 var hostname = '127.0.0.1';
 var port = 3000;
 
+// var routes = [
+//   { method: 'GET', path: '/', content: 'title'}
+// ];
+
 var server = http.createServer(function(req, res) {
+  var method = req.method;
+  var url = req.url;
+  // When a user visits '/'
+  // Show a page that displays a title and an image
+  // of your favorite actor or actress
   console.log('Incoming ', req.method, ' request to ', req.url);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  if ( method === 'GET' && url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+  } else {
+    res.statusCode = 404;
+    res.write('try again');
+    res.end();
+  }
 });
 
 server.listen(port, hostname, function() {
